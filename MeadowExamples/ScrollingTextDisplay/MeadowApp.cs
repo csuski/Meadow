c3ccc1a02d7d46650512e528ed3cdb643bac5543
Christopher Suski <1329211+csuski@users.noreply.github.com>
2019-11-26 10:26:33 -0700

    Initial example solution and structure

---
+++ MeadowExamples/ScrollingTextDisplay/MeadowApp.cs
@@ -0,0 +1,45 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Displays.Tft;
+using Meadow.Foundation.Graphics;
+using Meadow.Hardware;
+
+namespace ScrollingTextDisplay
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+        private ST7789 display;
+        private ISpiBus spiBus;
+        private GraphicsLibrary graphicsLib;
+
+        public MeadowApp()
+        {
+            ConfigurePorts();
+            DrawText();
+        }
+
+        public void ConfigurePorts()
+        {
+            var config = new SpiClockConfiguration(6000, SpiClockConfiguration.Mode.Mode3);
+            spiBus = Device.CreateSpiBus(Device.Pins.SCK, Device.Pins.MOSI, Device.Pins.MISO, config);
+            display = new ST7789(device: Device, spiBus: spiBus,
+               chipSelectPin: Device.Pins.D02,
+               dcPin: Device.Pins.D01,
+               resetPin: Device.Pins.D00,
+               width: 135, height: 240);
+
+            graphicsLib = new GraphicsLibrary(display);
+            graphicsLib.CurrentFont = new Font8x8();
+
+            graphicsLib.Clear();
+            graphicsLib.CurrentRotation = GraphicsLibrary.Rotation._90Degrees;
+            display.Clear(Meadow.Foundation.Color.Black, true);
+        }
+
+        public void DrawText()
+        {
+            graphicsLib.DrawText(0, 0, "test text", Meadow.Foundation.Color.White);
+            graphicsLib.Show();
+        }
+    }
+}
